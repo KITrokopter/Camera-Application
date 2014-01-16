be5a0c41227b2c9a437368661e6c85e6b434c8a2
Luchs <Luchs@euirc.eu>
2014-01-16 19:35:20 +0100

    main: Initialize ROS communication
    This removes the Kinect stuff as I cannot test this.

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,23 @@
 //#include <opencv2/core/mat.hpp>
 #include <iostream>
 
+#include "ros/ros.h"
+
 #include "libfreenect.hpp"
 #include "CvKinect.hpp"
 #include "ImageAnalyzer.hpp"
+#include "Communicator.hpp"
 
-int main(void) {
+int main(int argc, char **argv) {
 	std::cout << "Starting Camera Application" << std::endl;
 	
-	Freenect::Freenect freenect;
-	CvKinect& device = freenect.createDevice<CvKinect>(0);
-	ImageAnalyzer analyzer(&device);
+	/* Freenect::Freenect freenect; */
+	/* CvKinect& device = freenect.createDevice<CvKinect>(0); */
+	/* ImageAnalyzer analyzer(&device); */
 	
 	// Do ROS stuff
+	ros::init(argc, argv, "camera_application");
+	Communicator comm;
+	ROS_INFO("Initialized.");
+	ros::spin();
 }
